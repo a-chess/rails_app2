a28328d97b3f06fcadf29033166ee583fba29f32
hyuga <hyuga3110@gmail.com>
2018-06-10 23:28:09 +0900

    material design?

--- app/helpers/expences_helper.rb
+++ app/helpers/expences_helper.rb
@@ -2,7 +2,7 @@ module ExpencesHelper
   
   #対象月（YYYYMM)を元に月内の日付の配列を取得
   def get_days(target_month)
-    days = Array.new
+    days = []
     date_s = Date.new(target_month[0,4].to_i,target_month[4,2].to_i,1)
     date_e = Date.new(target_month[0,4].to_i,target_month[4,2].to_i,-1)
     
